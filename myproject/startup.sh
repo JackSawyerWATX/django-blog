c357b9eb3479ac21fc4cd36b102e5e948c69a786
John Doe <jack.h.sawyer@pm.me>
2023-11-28 19:51:07 -0800

    added to startup.sh

--- myproject/startup.sh
+++ myproject/startup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+
+# Set environment variables
+export DJANGO_SETTINGS_MODULE="your_project.settings"
+export DATABASE_URL="your_database_url"
+export SECRET_KEY="your_secret_key"
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the application
+gunicorn your_project.wsgi:application
